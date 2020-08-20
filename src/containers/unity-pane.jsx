d7857e90d298193b6c8783a75d587832739bab0b
Joshua Jarvis <bynarlogic@gmail.com>
2020-08-20 17:09:11 -0400

    calling attach unity on the redux vm.runtime instance

--- src/containers/unity-pane.jsx
+++ src/containers/unity-pane.jsx
@@ -3,25 +3,31 @@ import { connect } from "react-redux";
 import Unity from "react-unity-webgl";
 
 class UnityPane extends React.Component {
+  componentDidMount() {
+    const { unity, vm } = this.props;
+    unity.on("MessageFromUnity", () => {
+      console.log("Unity -> React");
+    });
 
-componentDidMount() {
-  this.props.unity.on('MessageFromUnity', () => {console.log("Unity -> React");})
-}
+    vm.runtime.attachUnity(unity);
+  }
 
-render() {
-  return (
-    <div className="unity-container">
-      <h2>Unity Content</h2>
-      <Unity unityContent={this.props.unity} />
-    </div>
-  );
-}
+  render() {
+    return (
+      <div className="unity-container">
+        <h2>Unity Content</h2>
+        <Unity unityContent={this.props.unity} />
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state) => {
   const unity = state.scratchGui.unity;
+  const vm = state.scratchGui.vm;
   return {
     unity: unity,
+    vm: vm,
   };
 };
 
